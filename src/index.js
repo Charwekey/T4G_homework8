262a311488be911c98ed641bb7cfaabd8afb81d9
Charwekey <charwekeyrabisabutey@gmail.com>
2024-12-04 21:38:49 +1200

    Added all files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ function checkGender(){
     let user = prompt("What is you gender?");
 
     if (user == "Male"){
-        alert("You are a boy");
+        alert("You are a boy!👨‍🦰");
 
     }
 
     else if (user =="Female"){
-        alert("You are a girl");
+        alert("You are a girl!👩‍🦰");
     }
     else{
         alert ("Invail input, try again");
